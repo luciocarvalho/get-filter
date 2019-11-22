77c27f536815c8c030876ff36ca9b937f6c513e6
Lucio Carvalho <lucio.carvalho.197@gmail.com>
2019-11-21 21:20:47 -0300

    Update get-filter.go

--- get-filter.go
+++ get-filter.go
@@ -13,39 +13,59 @@ import (
 )
 
 func getsudo() {
+
+}
+
+var (
+	//sudoCount = promauto.NewCounter(prometheus.CounterOpts{
+	sudoCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "get_sudo_count",
+		Help: "The total number of processed sudo events",
+		//	Buckets: prometheus.LinearBuckets(0.01, 0.01, 10),
+	})
+)
+
+func main() {
+	//getsudo()
 	go func() {
 		for {
+			sudoCount.Set(0)
 			f, err := ioutil.ReadFile("/var/log/secure")
+			//f, err := os.OpenFile("/var/log/secure", os.O_RDONLY, os.ModePerm)
 			if err != nil {
 				fmt.Println("Error to read file")
 				return
 			}
+			//defer f.Close()
 			str := string(f)
+			//str := bufio.NewScanner(f)
+
+			/*for str.Scan() {
+				_ = str.Text()
+				fmt.Println("teste")
+			}*/
 			r, err := regexp.Compile("sudo")
 			if err != nil {
 				fmt.Println("Error to compile regex")
 				return
 			}
-			c := len(r.FindAllString(str, -1))
+			//fmt.Println(str)
+			//c := len(r.FindAllString(str, -1))
+			c := len(r.FindAllStringSubmatchIndex(str, -1))
+
+			flow := float64(c)
+
+			sudoCount.Add(flow)
+			//sudoCount.SetToCurrentTime()
 
 			//con, err := strconv.ParseInt(c, 10, 64)
-			fmt.Println(c)
+			//			fmt.Println(c)
 			// Incrementart
 			//fmt.Println(len(c))
 			time.Sleep(2 * time.Second)
 		}
+
 	}()
-}
-
-var (
-	c = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "get_sudo_count",
-		Help: "The total number of processed sudo events",
-	})
-)
-
-func main() {
-	getsudo()
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
